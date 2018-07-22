64a909c6de26622a7efbdd5f9c08d98b5d69f8bd
Julianna Tetreault <juliannatetreault@icloud.com>
2018-07-22 09:12:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,3 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
  return playlist
 }
+
+function removeFromPlaylist(playlist, artistName, songTitle) {
+  
+}
