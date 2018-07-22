070d8c42075338c20c97adaf161c14bd75f4e3e4
Julianna Tetreault <juliannatetreault@icloud.com>
2018-07-22 09:03:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object({
-  artistName: 'song titles'
+  ['artistName']: 'song title'
 });
 
-function updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi") {
+function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, {['Phil Ochs']: "Here's to the State of Mississippi"})
 }
